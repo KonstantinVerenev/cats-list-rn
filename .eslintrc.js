635434c842b596cd88331eabbf083847f4d8c9e8
kverenev <kverenev@gmail.com>
2021-11-17 20:18:48 +0300

    Configure ESLint and Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,29 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  "env": {
+    "es2021": true,
+    "node": true,
+    "react-native/react-native": true
+  },
+  "extends": [
+    "plugin:react/recommended",
+    "airbnb",
+    "airbnb/hooks",
+    "prettier",
+    "prettier/react"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "react-native",
+    "@typescript-eslint"
+  ],
+  "rules": {
+  }
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  bracketSpacing: false,
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
-  arrowParens: 'avoid',
+  printWidth: 100,
+  arrowParens: 'always',
+  tabWidth: 2,
 };
